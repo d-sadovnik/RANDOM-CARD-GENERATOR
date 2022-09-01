6e40d496781f81556549d5fa0177aa89e41eb5df
d-sadovnik <borixe@gmail.com>
2022-09-01 01:31:57 +0000

    commti todos los cambios ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,24 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let simbolo = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let random = Math.floor(Math.random() * simbolo.length);
+
+  document.querySelector("#numero").append(simbolo[random]);
+
+  let cardsymbol = ["♣", "♠", "♥", "♦"];
+
+  let randomcardsymbol = Math.floor(Math.random() * cardsymbol.length);
+
+  document.querySelector("#symbol").append(cardsymbol[randomcardsymbol]);
+  document.querySelector("#symbol2").append(cardsymbol[randomcardsymbol]);
+
+  if (
+    cardsymbol[randomcardsymbol] === "♥" ||
+    cardsymbol[randomcardsymbol] === "♦"
+  ) {
+    document.querySelector("#symbol").style.color = "red";
+    document.querySelector("#symbol2").style.color = "red";
+  }
 };
